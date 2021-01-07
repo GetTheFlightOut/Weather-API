c098a10f7f18fb94c4016cbdde373847cb09821e
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-06 20:34:51 -0700

    Fix merge conflicts
    Co-authored-by: George Soderholm <62966396+GeorgieGirl24@users.noreply.github.com>
    Co-authored-by: Sheryl Stillman <7945439+stillsheryl@users.noreply.github.com>
    Co-authored-by: Todd Estes <66448493+Todd-Estes@users.noreply.github.com>

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -6,11 +6,28 @@ end
 
 describe ApplicationController do
   it "responds with the JSON requested" do
-    get '/'
+    get 'api/v1/weather?lat=35&lon=139'
+
     expect(last_response.status).to eq(200)
 
-    response = JSON.parse(last_response.body)["data"]
+    expect(last_response).to be_successful
+    response = JSON.parse(last_response.body, symbolize_names: true)[:data]
 
     expect(response).to be_a(Hash)
+
+    expect(response).to have_key(:lat)
+    expect(response[:lat]).to be_an(Integer)
+
+    expect(response).to have_key(:lon)
+    expect(response[:lon]).to be_a(Integer)
+
+    expect(response).to have_key(:timezone)
+    expect(response[:timezone]).to be_a(String)
+
+    expect(response).to have_key(:timezone_offset)
+    expect(response[:timezone_offset]).to be_a(Integer)
+
+    expect(response).to have_key(:current)
+    expect(response[:current]).to be_a(Hash)
   end
 end
