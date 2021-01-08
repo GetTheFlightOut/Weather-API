eba369722b21818e28d6ea0a81f15ed018f84be1
Will Dunlap <dunlapww@gmail.com>
2021-01-08 16:09:02 -0700

    Add new attributes to serializer
    Co-authored-by: Brian Liu <badgerbreezy@gmail.com>

--- app/serializers/weather_serializer.rb
+++ app/serializers/weather_serializer.rb
@@ -4,10 +4,14 @@ class WeatherSerializer
     json[:data] = []
     weather_days.each do |day|
       weather_day = {type: "weather", id: nil, attributes: {}}
-      weather_day[:attributes][:min_temp] = day.min_temp
-      weather_day[:attributes][:max_temp] = day.max_temp
+      weather_day[:attributes][:min_f] = day.min_f
+      weather_day[:attributes][:max_f] = day.max_f
+      weather_day[:attributes][:min_c] = day.min_c
+      weather_day[:attributes][:max_c] = day.max_c
+      weather_day[:attributes][:day_feels_like_c] = day.day_feels_like_c
+      weather_day[:attributes][:day_feels_like_f] = day.day_feels_like_f
       weather_day[:attributes][:description] = day.description
-      # weather_day[:attributes][:date] = day.date
+      weather_day[:attributes][:date] = day.date
       json[:data] << weather_day
     end
     json
