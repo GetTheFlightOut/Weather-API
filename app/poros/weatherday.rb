bafa6bfcded5e830439896595797ffb89cf9f287
Will Dunlap <dunlapww@gmail.com>
2021-01-08 15:13:29 -0700

    Add new attributes to weather poro
    Co-authored-by: Brian Liu <badgerbreezy@gmail.com>

--- app/poros/weatherday.rb
+++ app/poros/weatherday.rb
@@ -1,14 +1,23 @@
 class WeatherDay
-  attr_reader :min_temp,
-              :max_temp,
+  attr_reader :min_f,
+              :max_f,
+              :min_c,
+              :max_c,
+              :day_feels_like_f,
+              :day_feels_like_c,
               :description,
               :date
 
+
   def initialize(day_weather)
-    @min_temp = day_weather[:temp][:min]
-    @max_temp = day_weather[:temp][:max]
+    @min_f = fahrenheit(day_weather[:temp][:min])
+    @max_f = fahrenheit(day_weather[:temp][:max])
+    @min_c = celsius(day_weather[:temp][:min])
+    @max_c = celsius(day_weather[:temp][:max])
+    @day_feels_like_f = fahrenheit(day_weather[:feels_like][:day])
+    @day_feels_like_c = celsius(day_weather[:feels_like][:day])
     @description = day_weather[:weather][0][:description]
-    @date = DateTime.strptime(day_weather[:dTime].to_s.'%s')
+    @date = DateTime.strptime(day_weather[:dt].to_s,'%s')
   end
 
   def fahrenheit(temp)
