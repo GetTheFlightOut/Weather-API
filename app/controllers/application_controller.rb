21083f45fc90281684bbcc5a5b8bbf8945bb199b
Brian Liu <brian.b.liu@gmail.com>
2021-01-07 17:47:10 -0700

    Refactored connection and data parse into weather service
    Co-authored-by: Will Dunlap <53274357+dunlapww@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,9 @@
 require 'pry'
 require './config/environment'
+require './app/services/weather_service'
 
 class ApplicationController < Sinatra::Base
-
-  get "/api/v1/weather" do
-    # params = {"lat"=>"35", "lon"=>"139"}
-    lat = params[:lat]
-    lon = params[:lon]
-    conn = Faraday.new(url: "http://api.openweathermap.org")
-    
-    response = conn.get("/data/2.5/onecall") do |req|
-      req.params["lat"] = lat
-      req.params["lon"] = lon
-      req.params["appid"] = ENV['WEATHER_API_KEY']
-    end
-    # require 'pry'; binding.pry
-
-    data = JSON :data => JSON.parse(response.body)
+  get '/api/v1/weather' do
+    data = WeatherService.weather_search(params)
   end
 end
