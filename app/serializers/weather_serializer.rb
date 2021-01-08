5da8f8545d9f598e22549a643583cb21e7e32011
Brian Liu <brian.b.liu@gmail.com>
2021-01-07 21:18:40 -0700

    Create weather serializer
    Co-authored-by: Will Dunlap <53274357+dunlapww@users.noreply.github.com>

---
+++ app/serializers/weather_serializer.rb
@@ -0,0 +1,27 @@
+class WeatherSerializer
+  def self.new(weather_days)
+    json = {}
+    json[:data] = []
+    weather_days.each do |day|
+      weather_day = {type: "weather", id: nil, attributes: {}}
+      weather_day[:attributes][:min_temp] = day.min_temp
+      weather_day[:attributes][:max_temp] = day.max_temp
+      weather_day[:attributes][:description] = day.description
+      # weather_day[:attributes][:date] = day.date
+      json[:data] << weather_day
+    end
+    json
+  end
+end
+
+# data: {
+#   type: "weather",
+#   id: "123",
+#   attributes: {
+#     min_temp: "37",
+#     max_temp: "40",
+#     description: "cloudy",
+#     date: "05-01-2021"
+#     }
+#   }
+# }
