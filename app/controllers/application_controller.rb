5506423c73f72f2a15d4310076e477a6dde88794
Brian Liu <brian.b.liu@gmail.com>
2021-01-07 21:20:20 -0700

    Add serializer functionality to application controller
    Co-authored-by: Will Dunlap <53274357+dunlapww@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ require './app/services/weather_service'
 
 class ApplicationController < Sinatra::Base
   get '/api/v1/weather' do
-    data = WeatherService.weather_search(params)
+    @weather_days = WeatherFacade.daily_weather(params)
+    JSON WeatherSerializer.new(@weather_days)
   end
 end
