bd6d6f4e473a87d6ee82437bc528fc90503fe10d
Will Dunlap <dunlapww@gmail.com>
2021-01-08 17:24:05 -0700

    Add sad path test for weather api response
    Co-authored-by: Brian Liu <badgerbreezy@gmail.com>

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -1,38 +1,44 @@
-require_relative "spec_helper"
+require_relative 'spec_helper'
 
 def app
   ApplicationController
 end
 
 describe ApplicationController do
-  it "responds with the JSON requested" do
+  it 'responds with the JSON requested' do
     get 'api/v1/weather?lat=35&lon=139'
 
     expect(last_response.status).to eq(200)
-
     expect(last_response).to be_successful
-    response = JSON.parse(last_response.body, symbolize_names: true)[:data]
+    response = JSON.parse(last_response.body, symbolize_names: true)
 
     expect(response).to be_a(Hash)
 
-    expect(response).to have_key(:lat)
-    expect(response[:lat]).to be_an(Integer)
+    expect(response[:data][0][:attributes][:min_f]).to be_an(Float)
 
-    expect(response).to have_key(:lon)
-    expect(response[:lon]).to be_a(Integer)
+    expect(response[:data][0][:attributes][:max_f]).to be_an(Float)
 
-    expect(response).to have_key(:timezone)
-    expect(response[:timezone]).to be_a(String)
+    expect(response[:data][0][:attributes][:min_c]).to be_an(Float)
 
-    expect(response).to have_key(:timezone_offset)
-    expect(response[:timezone_offset]).to be_a(Integer)
+    expect(response[:data][0][:attributes][:max_c]).to be_an(Float)
 
-    expect(response).to have_key(:current)
-    expect(response[:current]).to be_a(Hash)
-    binding.pry
+    expect(response[:data][0][:attributes][:day_feels_like_f]).to be_an(Float)
+
+    expect(response[:data][0][:attributes][:day_feels_like_c]).to be_an(Float)
+
+    expect(response[:data][0][:attributes][:description]).to be_an(String)
+
+    expect(response[:data][0][:attributes][:date]).to be_a(String)
   end
 
-  it 'returns error message and status code with invalid params' do
+  it 'it return an error json if invalid lat/lon entered' do
+    get 'api/v1/weather?lat=35&lon=ab'
 
+    #expecting below structure for error JSON
+    #{:cod=>"400", :message=>"wrong longitude"}
+    require 'pry'; binding.pry
+    response = JSON.parse(last_response.body, symbolize_names: true)
+    expect(response[:cod]).to eq('400')
+    expect(response).to have_key(:message)
   end
 end
