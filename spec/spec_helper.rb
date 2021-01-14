72cc4262acf607264ca237375c3974377e2649fb
Nick King <nickmaxking@gmail.com>
2021-01-14 15:14:59 -0700

    Move SimpleCov setup placement
    Co-authored-by: Brian Liu <67594471+badgerbreezy@users.noreply.github.com>
    Co-authored-by: Todd Estes <66448493+Todd-Estes@users.noreply.github.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 ENV['SINATRA_ENV'] = 'test'
+require 'simplecov'
+SimpleCov.start
 
 require_relative '../config/environment'
 require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
-require 'simplecov'
-SimpleCov.start
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
