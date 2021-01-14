ea0845d5c3899fd99e0a50d7db459d1b135d5496
Nick King <nickmaxking@gmail.com>
2021-01-14 15:55:33 -0700

    Add more sad path tests
    Co-authored-by: Brian Liu <67594471+badgerbreezy@users.noreply.github.com>
    Co-authored-by: Todd Estes <66448493+Todd-Estes@users.noreply.github.com>

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -33,11 +33,30 @@ describe ApplicationController do
     expect(response[:data][0][:attributes][:sky_coverage]).to be_a(Integer)
   end
 
-  it 'it return an error json if invalid lat/lon entered' do
+  it 'it return an error json if invalid lon entered' do
     get 'api/v1/weather?lat=35&lon=ab'
 
     response = JSON.parse(last_response.body, symbolize_names: true)
     expect(response[:cod]).to eq('400')
     expect(response).to have_key(:message)
+    expect(response[:message]).to eq('wrong longitude')
+  end
+
+  it 'it return an error json if invalid lat entered' do
+    get 'api/v1/weather?lat=ab&lon=35'
+
+    response = JSON.parse(last_response.body, symbolize_names: true)
+    expect(response[:cod]).to eq('400')
+    expect(response).to have_key(:message)
+    expect(response[:message]).to eq('wrong latitude')
+  end
+
+  it 'it return an error json if invalid lat and lon entered' do
+    get 'api/v1/weather?lat=ab&lon=ab'
+
+    response = JSON.parse(last_response.body, symbolize_names: true)
+    expect(response[:cod]).to eq('400')
+    expect(response).to have_key(:message)
+    expect(response[:message]).to eq('wrong latitude')
   end
 end
